19a6f707cdbff8817d7d740bee8293a8610e355a
drifterz13 <mixmyxxe@gmail.com>
2026-02-01 10:44:40 +0700

    ci: trigger deploy

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -12,10 +12,7 @@ export function App() {
         <img src={reactLogo} alt="React Logo" className="logo react-logo" />
       </div>
 
-      <h1>SCOS</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
-      </p>
+      <h1>SCOS App</h1>
       <APITester />
     </div>
   );
